56d6c778682483554e258b69f7ec88fe7bc31f3a
Noam <noam.ascher@gmail.com>
2016-11-28 20:48:26 +0000

    Cart is Empty feature implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,38 @@
 class ApplicationController < ActionController::Base
+
+
+  def index
+    render plain: "Everyone can see me!"
+  end
+
+  def edit
+    render plain: "I'm only accessible if you know the password"
+  end
+  # require 'bcrypt'
+
+  # class PasswordDigester
+  #   def self.encrypt(password)
+  #     BCrypt::Password.create(password)
+  #   end
+
+  #   def self.check?(password, encrypted_password)
+  #     BCrypt::Password.new(encrypted_password) == password
+  #   end
+  # end
+
+  # # For testing
+  # if $0 == __FILE__
+  #   password = ARGV.shift
+
+  #   encrypted = PasswordDigester.encrypt password
+
+  #   success = PasswordDigester.check? password, encrypted
+
+  #   puts encrypted, success
+  # end
+
+
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
