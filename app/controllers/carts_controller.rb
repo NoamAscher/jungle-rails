86f75722a4feb12ec0166dca76798d0db80dfa77
Noam <noam.ascher@gmail.com>
2016-12-01 19:14:32 +0000

    added flash message from controller for when an item is sold out

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,14 +4,19 @@ class CartsController < ApplicationController
   end
 
   def add_item
+
     product_id = params[:product_id].to_s
 
-    item = cart[product_id] || { "quantity" => 0 }
-    item["quantity"] += 1
-    cart[product_id] = item
-    update_cart cart
+    if Product.find(product_id).quantity == 0
+      redirect_to :back, notice: "Item is sold out."
+    else
+      item = cart[product_id] || { "quantity" => 0 }
+      item["quantity"] += 1
+      cart[product_id] = item
+      update_cart cart
 
-    redirect_to :back
+      redirect_to :back
+    end
   end
 
   def remove_item
